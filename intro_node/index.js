1f9f382003c4c01b67555a470a4042e8c94cc701
Mohamed El garraoui <154738113+mel-garr@users.noreply.github.com>
2025-01-02 03:43:54 +0000

    cli up

--- intro_node/index.js
+++ intro_node/index.js
@@ -1,2 +1,10 @@
+#!/usr/bin/env node
 console.log('hello world')
-alert('hello world')
+const note = process.argv[2]
+console.log(note)
+const newnote = {
+    'Content' : note,
+    'Date'    : Date.now() 
+}
+console.log(newnote)
+// console.log(process.pid)
